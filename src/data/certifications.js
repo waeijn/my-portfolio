23388133031858a9e3c29de90e5185e760b48837
John Wayne Landong <johnwaynelandong@gmail.com>
2026-01-09 22:24:38 +0800

    feat: add new certification and reorder list

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,6 +1,16 @@
 export const certifications = [
   {
     id: 1,
+    title: "Certified Full Stack Developer",
+    issuer: "freeCodeCamp",
+    date: "In Progress",
+    description:
+      "Extensive curriculum covering responsive web design, JavaScript algorithms, and full-stack integration using the MERN stack.",
+    skills: ["React.js", "Node.js", "MongoDB", "Data Structures"],
+    link: "",
+  },
+  {
+    id: 2,
     title: "Networking Basics",
     issuer: "Cisco Networking Academy",
     date: "January 2026",
@@ -10,7 +20,22 @@ export const certifications = [
     link: "https://www.credly.com/badges/9ba7b187-046c-4732-873d-c0ef11275298/public_url",
   },
   {
-    id: 2,
+    id: 3,
+    title: "Introduction to Cybersecurity Tools & Cyberattacks",
+    issuer: "IBM",
+    date: "January 2026",
+    description:
+      "Explored the history of cybersecurity, types of cyberattacks, and key tools for protecting digital assets and organizational data.",
+    skills: [
+      "Cyber Attacks",
+      "Security Awareness",
+      "Threat Management",
+      "Authentications",
+    ],
+    link: "https://www.coursera.org/account/accomplishments/records/CJQ6S19BLW6A",
+  },
+  {
+    id: 4,
     title: "Introduction to Cybersecurity",
     issuer: "Cisco Networking Academy",
     date: "December 2025",
@@ -18,27 +43,6 @@ export const certifications = [
       "Achieved student-level credential for explaining common cyber threats, attacks, vulnerabilities, and protection methods for individuals and organizations.",
     skills: ["Cybersecurity", "Networking", "Cyber Threats", "Online Safety"],
     link: "https://www.credly.com/badges/f30e0048-38ba-4646-b125-c51baa80e5a7/public_url",
-  },
-
-  {
-    id: 3,
-    title: "Legacy Responsive Web Design V8",
-    issuer: "freeCodeCamp",
-    date: "December 2024",
-    description:
-      "A comprehensive certification (approx. 300 hours) covering modern responsive web design principles, HTML5, and CSS3.",
-    skills: ["HTML5", "CSS3", "Responsive Design", "Web Accessibility"],
-    link: "https://freecodecamp.org/certification/waeijn/responsive-web-design",
-  },
-  {
-    id: 4,
-    title: "Certified Full Stack Developer",
-    issuer: "freeCodeCamp",
-    date: "In Progress",
-    description:
-      "Extensive curriculum covering responsive web design, JavaScript algorithms, and full-stack integration using the MERN stack.",
-    skills: ["React.js", "Node.js", "MongoDB", "Data Structures"],
-    link: "",
   },
   {
     id: 5,
@@ -59,5 +63,15 @@ export const certifications = [
       "Completed coursework focused on applying Human-Centered Design principles to problem-solving and product development.",
     skills: ["Human-Centered Design (HCD)", "UX Principles", "Design Thinking"],
     link: "https://www.canva.com/design-school/certification-award/56a4e5af-d84d-4869-b54a-f73705915ff1",
+  },
+  {
+    id: 7,
+    title: "Legacy Responsive Web Design V8",
+    issuer: "freeCodeCamp",
+    date: "December 2024",
+    description:
+      "A comprehensive certification (approx. 300 hours) covering modern responsive web design principles, HTML5, and CSS3.",
+    skills: ["HTML5", "CSS3", "Responsive Design", "Web Accessibility"],
+    link: "https://freecodecamp.org/certification/waeijn/responsive-web-design",
   },
 ];
